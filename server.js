6c7352e540c426b4e43d24a06d3a32b63b629e66
eldar867 <eldar7012004@gmail.com>
2026-03-28 20:24:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+const fastify = require('fastify')({ logger: true });
+const path = require('path');
 
+fastify.register(require('@fastify/static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/',
+});
+
+fastify.get('/api', async (request, reply) => {
+  return { message: "Запрос прошел успешно" };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+    console.log('Сервер запущен на http://localhost:3000');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
